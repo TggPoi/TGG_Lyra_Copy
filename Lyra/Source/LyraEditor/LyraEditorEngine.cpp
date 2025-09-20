cec0b8b1f915c81a83bac79bc39fd67932d8ad20
TggPoi <2631925996@qq.com>
2025-09-20 17:59:01 +0800

    2-2.5 初始化自定义Engine类，配置ini文件

--- Lyra/Source/LyraEditor/LyraEditorEngine.cpp
+++ Lyra/Source/LyraEditor/LyraEditorEngine.cpp
@@ -2,3 +2,64 @@
 
 #include "LyraEditorEngine.h"
 
+#include "Development/LyraDeveloperSettings.h"
+#include "Development/LyraPlatformEmulationSettings.h"
+#include "Engine/GameInstance.h"
+#include "Framework/Notifications/NotificationManager.h"
+#include "GameModes/LyraWorldSettings.h"
+#include "Settings/ContentBrowserSettings.h"
+#include "Settings/LevelEditorPlaySettings.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LyraEditorEngine)
+class IEngineLoop;
+
+#define LOCTEXT_NAMESPACE "LyraEditor"
+
+ULyraEditorEngine::ULyraEditorEngine(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void ULyraEditorEngine::Init(IEngineLoop* InEngineLoop)
+{
+	Super::Init(InEngineLoop);
+}
+
+void ULyraEditorEngine::Start()
+{
+	Super::Start();
+}
+
+void ULyraEditorEngine::Tick(float DeltaSeconds, bool bIdleMode)
+{
+	Super::Tick(DeltaSeconds, bIdleMode);
+
+	FirstTickSetup();
+}
+
+FGameInstancePIEResult ULyraEditorEngine::PreCreatePIEInstances(const bool bAnyBlueprintErrors,
+	const bool bStartInSpectatorMode, const float PIEStartTime, const bool bSupportsOnlinePIE,
+	int32& InNumOnlinePIEInstances)
+{
+	//可以在这里设置启动模式，如果启动模式错误就会弹出错误提示
+	FGameInstancePIEResult Result = Super::PreCreatePIEServerInstance(bAnyBlueprintErrors, bStartInSpectatorMode, PIEStartTime, bSupportsOnlinePIE, InNumOnlinePIEInstances);
+
+	return Result;
+}
+
+void ULyraEditorEngine::FirstTickSetup()
+{
+	if (bFirstTickSetup)
+	{
+		return;
+	}
+
+	bFirstTickSetup = true;
+
+	// Force show plugin content on load.
+	//让引擎执行第一帧时强制显示插件内容，而不需要每次都手动点击显示插件内容，方便后面使用Gamefeature
+	GetMutableDefault<UContentBrowserSettings>()->SetDisplayPluginFolders(true);
+	
+}
+#undef LOCTEXT_NAMESPACE
--- Lyra/Source/LyraGame/System/LyraGameEngine.cpp
+++ Lyra/Source/LyraGame/System/LyraGameEngine.cpp
@@ -1,3 +1,15 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LyraGameEngine.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LyraGameEngine)
+
+ULyraGameEngine::ULyraGameEngine(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void ULyraGameEngine::Init(IEngineLoop* InEngineLoop)
+{
+	Super::Init(InEngineLoop);
+}
