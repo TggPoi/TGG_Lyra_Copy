98df5da0de0afbc7127d947f95a835ac75193fa0
TggPoi <2631925996@qq.com>
2025-10-27 00:20:53 +0800

    7.2-玩家出生点类

--- Lyra/Source/LyraGame/Player/LyraPlayerStart.cpp
+++ Lyra/Source/LyraGame/Player/LyraPlayerStart.cpp
@@ -1,3 +1,94 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LyraPlayerStart.h"
+
+#include "Engine/World.h"
+#include "GameFramework/GameModeBase.h"
+#include "TimerManager.h"
+
+ALyraPlayerStart::ALyraPlayerStart(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
+
+ELyraPlayerStartLocationOccupancy ALyraPlayerStart::GetLocationOccupancy(AController* const ControllerPawnToFit) const
+{
+	UWorld* const World = GetWorld();
+	if (HasAuthority() && World)
+	{
+		if (AGameModeBase* AuthGameMode = World->GetAuthGameMode())
+		{
+			TSubclassOf<APawn> PawnClass = AuthGameMode->GetDefaultPawnClassForController(ControllerPawnToFit);
+			const APawn* const PawnToFit = PawnClass ? GetDefault<APawn>(PawnClass) : nullptr;
+
+			
+			FVector ActorLocation = GetActorLocation();
+			const FRotator ActorRotation = GetActorRotation();
+			/** 如果演员在测试位置处会因受到阻碍物的阻挡而被迫移动，则返回 true 。 同时还会返回一个可能使测试位置不再受阻碍的调整建议值。*/
+			if (!World->EncroachingBlockingGeometry(PawnToFit, ActorLocation, ActorRotation, nullptr))
+			{
+				// 可以完美的放进去
+				return ELyraPlayerStartLocationOccupancy::Empty;
+			}
+			/**
+			 * 尝试找到一个可接受且不会发生碰撞的位置来放置 TestActor，使其尽可能靠近 PlaceLocation。要求 PlaceLocation 必须是关卡内有效的位置。
+			 * 如果找到了一个没有阻碍碰撞的合适位置，则返回 true，此时 PlaceLocation 会被更新为新的无碰撞位置。
+			 * 如果未找到合适的位置，则返回 false，此时 PlaceLocation 不会改变。
+			 * 
+			 */
+			else if (World->FindTeleportSpot(PawnToFit, ActorLocation, ActorRotation))
+			{
+
+				// 需要移动部分位置
+				return ELyraPlayerStartLocationOccupancy::Partial;
+			}
+		
+		}
+		
+	}
+	
+
+	// 被完全阻塞了
+	return ELyraPlayerStartLocationOccupancy::Full;
+}
+
+bool ALyraPlayerStart::IsClaimed() const
+{
+	return ClaimingController != nullptr;
+}
+
+bool ALyraPlayerStart::TryClaim(AController* OccupyingController)
+{
+	if (OccupyingController != nullptr && !IsClaimed())
+	{
+		ClaimingController = OccupyingController;
+		if (UWorld* World = GetWorld())
+		{
+			// 这里需要有一个宣称判断
+			World->GetTimerManager().SetTimer(ExpirationTimerHandle,
+				FTimerDelegate::CreateUObject(this, &ALyraPlayerStart::CheckUnclaimed), ExpirationCheckInterval, true);
+			
+		}
+	}
+
+	return false;
+}
+
+void ALyraPlayerStart::CheckUnclaimed()
+{
+	if (ClaimingController != nullptr && ClaimingController->GetPawn() != nullptr && GetLocationOccupancy(ClaimingController) == ELyraPlayerStartLocationOccupancy::Empty)
+	{
+		// 证明已经使用完毕啦!
+		ClaimingController = nullptr;
+		
+		if (UWorld* World = GetWorld())
+		{
+			World->GetTimerManager().ClearTimer(ExpirationTimerHandle);
+		}
+
+		
+	}
+
+	
+}
--- Lyra/Source/LyraGame/Player/LyraPlayerStart.h
+++ Lyra/Source/LyraGame/Player/LyraPlayerStart.h
@@ -1,3 +1,85 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+// Finished.
 #pragma once
+
+#include "GameFramework/PlayerStart.h"
+#include "GameplayTagContainer.h"
+
+#include "LyraPlayerStart.generated.h"
+
+#define UE_API LYRAGAME_API
+class AController;
+class UObject;
+
+enum class ELyraPlayerStartLocationOccupancy
+{
+	// 空闲
+	Empty,
+	// 部分占用
+	Partial,
+	// 完全占用
+	Full
+};
+
+/**
+ * ALyraPlayerStart
+ * 
+ * Base player starts that can be used by a lot of modes.
+ * 基础角色可被多种模式所使用。
+ */
+UCLASS(MinimalAPI, Config = Game)
+class ALyraPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UE_API ALyraPlayerStart(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	// 获取目标位置的 是否可以放下Pawn的碰撞状态(大小是否合适).
+	UE_API ELyraPlayerStartLocationOccupancy GetLocationOccupancy(AController* const ControllerPawnToFit) const;
+
+	/** Did this player start get claimed by a controller already? */
+	// 是否被占用
+	UE_API bool IsClaimed() const;
+
+	
+	/** If this PlayerStart was not claimed, claim it for ClaimingController */
+	// 尝试占用
+	UE_API bool TryClaim(AController* OccupyingController);
+
+protected:
+
+	/** Check if this PlayerStart is still claimed */
+	/** 检查此玩家起点是否仍处于占用状态 */
+	UE_API void CheckUnclaimed();
+	
+	/** The controller that claimed this PlayerStart */
+	/** 承担此玩家起始点控制权的控制器 */
+	UPROPERTY(Transient)
+	TObjectPtr<AController> ClaimingController = nullptr;
+
+	
+	/** Interval in which we'll check if this player start is not colliding with anyone anymore */
+	/** 我们将在此间隔时间内检查此玩家的移动是否不再与任何人发生碰撞 */
+	UPROPERTY(EditDefaultsOnly, Category = "Player Start Claiming")
+	float ExpirationCheckInterval = 1.f;
+
+	/** Tags to identify this player start */
+	/** 用于标识此玩家的标签开始 */
+	UPROPERTY(EditAnywhere)
+	FGameplayTagContainer StartPointTags;
+
+	/** Handle to track expiration recurring timer */
+	/** 用于跟踪定期过期定时器的标识符 */
+	FTimerHandle ExpirationTimerHandle;
+	
+	
+	
+};
+
+
+
+
+
+#undef UE_API
