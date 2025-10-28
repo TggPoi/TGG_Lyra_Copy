83641a016ff521bbe2151ea7b07e7ab791e6e34e
TggPoi <2631925996@qq.com>
2025-10-29 00:47:16 +0800

    8.1-8.2 gamemode工作流程实现

--- Lyra/Source/LyraGame/GameModes/LyraGameMode.cpp
+++ Lyra/Source/LyraGame/GameModes/LyraGameMode.cpp
@@ -44,3 +44,648 @@ ALyraGameMode::ALyraGameMode(const FObjectInitializer& ObjectInitializer)
 	HUDClass = ALyraHUD::StaticClass();
 	
 }
+
+const ULyraPawnData* ALyraGameMode::GetPawnDataForController(const AController* InController) const
+{
+	// See if pawn data is already set on the player state
+	// 检查是否已为玩家状态设置了棋子数据
+
+	// 控制器不应该为空!
+	if (InController != nullptr)
+	{
+		// AI控制器也会有PlayerState,通过其的构造函数开启
+		if (const ALyraPlayerState* LyraPS = InController->GetPlayerState<ALyraPlayerState>())
+		{
+			//还没有实现
+			// if (const ULyraPawnData* PawnData = LyraPS->GetPawnData<ULyraPawnData>())
+			// {
+			// 	return PawnData;
+			// }
+		}
+	}
+
+
+	// 只有针对每个控制器第一次调用的时候,才会走到下面,PlayerState获取到自己的PawnData并从服务器初始化.
+	// 后续就直接从对应的PlayerState上取就好了
+
+	// If not, fall back to the the default for the current experience
+	// 如果不是这样，就回退到当前体验的默认设置。
+	
+	// 这个函数的执行时机必须在GameState及Experience加载完成之后!
+	check(GameState);
+	ULyraExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<ULyraExperienceManagerComponent>();
+	check(ExperienceComponent);
+	
+	// 因为只有在加载完成之后 才能确定这个关卡是哪一种玩法设计. PvE还是PvP.
+	if (ExperienceComponent->IsExperienceLoaded())
+	{
+		const ULyraExperienceDefinition* Experience = ExperienceComponent->GetCurrentExperienceChecked();
+
+		if (Experience->DefaultPawnData != nullptr)
+		{
+			// 这里是加载的指定体验的PawnData
+			return Experience->DefaultPawnData;
+			
+		}
+			
+		// Experience is loaded and there's still no pawn data, fall back to the default for now
+		// 已加载体验数据，但目前仍未获取到兵种数据，暂且使用默认值。
+
+		
+		// 这里获取的资产管理其里面配置在ini的默认全局PawnData
+		return ULyraAssetManager::Get().GetDefaultPawnData();
+		
+	}
+	
+	// Experience not loaded yet, so there is no pawn data to be had
+	// 体验数据尚未加载，因此目前无法获取任何角色数据
+	return nullptr;
+	
+}
+
+void ALyraGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+
+	// Wait for the next frame to give time to initialize startup settings
+	// 等待下一帧的到来，以便有足够的时间来初始化启动设置
+	GetWorld()->GetTimerManager().SetTimerForNextTick(this, &ThisClass::HandleMatchAssignmentIfNotExpectingOne);
+
+	
+}
+
+UClass* ALyraGameMode::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+	if (const ULyraPawnData* PawnData = GetPawnDataForController(InController))
+	{
+		if (PawnData->PawnClass)
+		{
+			return PawnData->PawnClass;
+		}
+	}
+
+	return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+APawn* ALyraGameMode::SpawnDefaultPawnAtTransform_Implementation(AController* NewPlayer,
+	const FTransform& SpawnTransform)
+{
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.Instigator = GetInstigator();
+	SpawnInfo.ObjectFlags |= RF_Transient;	// Never save the default player pawns into a map.// 请勿将默认的玩家棋子保存到地图中。
+	/* 决定是否运行构建脚本。如果为真，则不会在生成的角色上运行构建脚本。仅在角色是从蓝图中生成的情况下适用。*/
+	// 延迟构造Actor
+	SpawnInfo.bDeferConstruction = true;
+
+	if (UClass* PawnClass = GetDefaultPawnClassForController(NewPlayer))
+	{
+		if (APawn* SpawnedPawn = GetWorld()->SpawnActor<APawn>(PawnClass, SpawnTransform, SpawnInfo))
+		{
+			//TODO:我们现在还没有自定义角色的初始化过程
+			/* pawn的拓展组件用于验证当前的PawnData是否可用，确认可用的情况下才会继续往下执行流程，验证其他组件
+			if (ULyraPawnExtensionComponent* PawnExtComp = ULyraPawnExtensionComponent::FindPawnExtensionComponent(SpawnedPawn))
+			{
+				if (const ULyraPawnData* PawnData = GetPawnDataForController(NewPlayer))
+				{
+					// 很重要 传递了PawnData
+					PawnExtComp->SetPawnData(PawnData);
+				}
+				else
+				{
+					UE_LOG(LogLyra, Error, TEXT("Game mode was unable to set PawnData on the spawned pawn [%s]."), *GetNameSafe(SpawnedPawn));
+				}
+			}
+			*/
+
+			SpawnedPawn->FinishSpawning(SpawnTransform);
+			return SpawnedPawn;
+		}
+		else
+		{
+			UE_LOG(LogLyra, Error, TEXT("Game mode was unable to spawn Pawn of class [%s] at [%s]."), *GetNameSafe(PawnClass), *SpawnTransform.ToHumanReadableString());
+		}
+		
+	}
+	else
+	{
+		UE_LOG(LogLyra, Error, TEXT("Game mode was unable to spawn Pawn due to NULL pawn class."));
+	}
+		
+	return nullptr;
+}
+
+
+
+bool ALyraGameMode::ShouldSpawnAtStartSpot(AController* Player)
+{
+	// We never want to use the start spot, always use the spawn management component.
+	// 我们绝不会使用起始位置，而是始终使用生成管理组件。
+	return false;
+}
+
+void ALyraGameMode::HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer)
+{
+	// Delay starting new players until the experience has been loaded
+	// (players who log in prior to that will be started by OnExperienceLoaded)
+	// 在加载游戏体验之前，先延迟启动新玩家
+	// （在该过程之前登录的玩家将由“加载体验完成时启动”功能来启动）
+	if (IsExperienceLoaded())
+	{
+		Super::HandleStartingNewPlayer_Implementation(NewPlayer);
+	}
+}
+
+AActor* ALyraGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	if (ULyraPlayerSpawningManagerComponent* PlayerSpawningComponent = GameState->FindComponentByClass<ULyraPlayerSpawningManagerComponent>())
+	{
+		return PlayerSpawningComponent->ChoosePlayerStart(Player);
+	}
+	
+	return Super::ChoosePlayerStart_Implementation(Player);
+}
+
+void ALyraGameMode::FinishRestartPlayer(AController* NewPlayer, const FRotator& StartRotation)
+{
+	if (ULyraPlayerSpawningManagerComponent* PlayerSpawningComponent = GameState->FindComponentByClass<ULyraPlayerSpawningManagerComponent>())
+	{
+		PlayerSpawningComponent->FinishRestartPlayer(NewPlayer, StartRotation);
+	}
+
+	Super::FinishRestartPlayer(NewPlayer, StartRotation);
+
+	
+}
+
+bool ALyraGameMode::PlayerCanRestart_Implementation(APlayerController* Player)
+{
+	return ControllerCanRestart(Player);
+}
+
+void ALyraGameMode::InitGameState()
+{
+	Super::InitGameState();
+
+	// Listen for the experience load to complete
+	// 监听体验加载完成的进程
+	ULyraExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<ULyraExperienceManagerComponent>();
+	check(ExperienceComponent);
+	
+	ExperienceComponent->CallOrRegister_OnExperienceLoaded(FOnLyraExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::OnExperienceLoaded));
+
+}
+
+bool ALyraGameMode::UpdatePlayerStartSpot(AController* Player, const FString& Portal, FString& OutErrorMessage)
+{
+	// Do nothing, we'll wait until PostLogin when we try to spawn the player for real.
+	// Doing anything right now is no good, systems like team assignment haven't even occurred yet.
+
+	// 不做任何操作，我们先等到“登录完成”事件发生时再尝试真正生成玩家。
+	// 现在采取任何行动都不明智，因为诸如团队分配这类功能目前还未实现。
+	return true;
+}
+
+void ALyraGameMode::GenericPlayerInitialization(AController* NewPlayer)
+{
+	Super::GenericPlayerInitialization(NewPlayer);
+
+	
+	// 这个时候玩家控制器已经创建好了
+	// 这个代理由队伍组件来绑定,从而在这里嵌入队伍初始化的时机
+	OnGameModePlayerInitialized.Broadcast(this, NewPlayer);
+}
+
+void ALyraGameMode::FailedToRestartPlayer(AController* NewPlayer)
+{
+	Super::FailedToRestartPlayer(NewPlayer);
+
+	// If we tried to spawn a pawn and it failed, lets try again *note* check if there's actually a pawn class
+	// before we try this forever.
+	// 如果我们尝试生成一个兵，并且失败了，那就再试一次（注意）——在我们不断尝试之前，先要确认是否确实存在兵类。
+
+	if (UClass* PawnClass = GetDefaultPawnClassForController(NewPlayer))
+	{
+		if (APlayerController* NewPC = Cast<APlayerController>(NewPlayer))
+		{
+			// If it's a player don't loop forever, maybe something changed and they can no longer restart if so stop trying.
+			// 如果是玩家的话，不要一直循环下去，如果情况有所变化，他们可能无法重新开始游戏了，那么就停止尝试了。
+			if (PlayerCanRestart(NewPC))
+			{
+				RequestPlayerRestartNextFrame(NewPlayer, false);	
+			}
+			else
+			{
+				UE_LOG(LogLyra, Verbose, TEXT("FailedToRestartPlayer(%s) and PlayerCanRestart returned false, so we're not going to try again."), *GetPathNameSafe(NewPlayer));
+
+			}
+			
+		}
+		else
+		{
+			RequestPlayerRestartNextFrame(NewPlayer, false);
+		}
+
+
+		
+	}
+	else
+	{
+		UE_LOG(LogLyra, Verbose, TEXT("FailedToRestartPlayer(%s) but there's no pawn class so giving up."), *GetPathNameSafe(NewPlayer));
+	}
+	
+
+
+	
+	
+}
+
+void ALyraGameMode::RequestPlayerRestartNextFrame(AController* Controller, bool bForceReset)
+{
+	if (bForceReset && (Controller != nullptr))
+	{
+		Controller->Reset();
+	}
+	
+	if (APlayerController* PC = Cast<APlayerController>(Controller))
+	{
+		// 通知玩家重新生成控制对象
+		GetWorldTimerManager().SetTimerForNextTick(PC, &APlayerController::ServerRestartPlayer_Implementation);
+	}
+	
+	//@XGTODO:还没有实现机器人的控制器
+	/*else if (ALyraPlayerBotController* BotController = Cast<ALyraPlayerBotController>(Controller))
+	{
+		// 通知AI控制器重新生成对象
+		GetWorldTimerManager().SetTimerForNextTick(BotController, &ALyraPlayerBotController::ServerRestartController);
+	}*/
+	
+}
+
+bool ALyraGameMode::ControllerCanRestart(AController* Controller)
+{
+	if (APlayerController* PC = Cast<APlayerController>(Controller))
+	{	
+		if (!Super::PlayerCanRestart_Implementation(PC))
+		{
+			return false;
+		}
+	}
+	else
+	{
+		// Bot version of Super::PlayerCanRestart_Implementation
+		// 机器人玩家可重置功能的实现代码
+		if ((Controller == nullptr) || Controller->IsPendingKillPending())
+		{
+			return false;
+		}
+	}		
+
+	if (ULyraPlayerSpawningManagerComponent* PlayerSpawningComponent = GameState->FindComponentByClass<ULyraPlayerSpawningManagerComponent>())
+	{
+		return PlayerSpawningComponent->ControllerCanRestart(Controller);
+	}
+
+	return true;
+	
+}
+
+
+void ALyraGameMode::OnExperienceLoaded(const ULyraExperienceDefinition* CurrentExperience)
+{
+	// Spawn any players that are already attached
+	// 生成所有已附着的玩家对象
+
+	//@TODO: Here we're handling only *player* controllers, but in GetDefaultPawnClassForController_Implementation we skipped all controllers
+	//@待办事项：在这里我们只处理玩家控制器，但在 GetDefaultPawnClassForController_Implementation 函数中我们忽略了所有控制器。
+	// GetDefaultPawnClassForController_Implementation might only be getting called for players anyways
+	// “GetDefaultPawnClassForController_Implementation” 方法实际上可能只是在为玩家调用的，所以这种情况是完全有可能发生的。
+
+	// 比如AI控制呢?
+
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		APlayerController* PC = Cast<APlayerController>(*Iterator);
+		if ((PC != nullptr) && (PC->GetPawn() == nullptr))
+		{
+			if (PlayerCanRestart(PC))
+			{
+				RestartPlayer(PC);
+			}
+			
+		}
+	}
+
+}
+
+bool ALyraGameMode::IsExperienceLoaded() const
+{
+	check(GameState);
+	ULyraExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<ULyraExperienceManagerComponent>();
+	check(ExperienceComponent);
+
+	return ExperienceComponent->IsExperienceLoaded();
+	
+}
+
+void ALyraGameMode::OnMatchAssignmentGiven(FPrimaryAssetId ExperienceId, const FString& ExperienceIdSource)
+{
+	if (ExperienceId.IsValid())
+	{
+		UE_LOG(LogLyraExperience, Log, TEXT("Identified experience %s (Source: %s)"), *ExperienceId.ToString(), *ExperienceIdSource);
+
+		ULyraExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<ULyraExperienceManagerComponent>();
+		check(ExperienceComponent);
+		ExperienceComponent->SetCurrentExperience(ExperienceId);
+		
+	}
+	else
+	{
+		// 体验加载失败,前端界面会一直卡在Loading界面
+		UE_LOG(LogLyraExperience, Error, TEXT("Failed to identify experience, loading screen will stay up forever"));
+	}
+	
+	
+}
+
+void ALyraGameMode::HandleMatchAssignmentIfNotExpectingOne()
+{
+	// 1.配对任务分配（若有）
+	
+	// 准备好要使用的体验资产ID
+	FPrimaryAssetId ExperienceId;
+
+	// 体验的来源
+	FString ExperienceIdSource;
+
+	// Precedence order (highest wins)
+	//  - Matchmaking assignment (if present)
+	//  - URL Options override
+	//  - Developer Settings (PIE only)
+	//  - Command Line override
+	//  - World Settings
+	//  - Dedicated server
+	//  - Default experience
+	
+	// experience任务执行的 优先级顺序（以最高优先级为准），获取最终的ExperienceIdSource，优先级从上往下递增
+	//  - 配对任务分配（若有）
+	//  - URL 选项覆盖
+	//  - 开发者设置（仅适用于 PIE）
+	//  - 命令行覆盖
+	//  - 世界设置
+	//  - 专用服务器
+	//  - 默认experience
+
+	UWorld* World = GetWorld();
+
+	// 2.URL 选项覆盖
+	// 这里看一下GameMode里面OptionsString是否有被在其他地方赋值修改 能否直接获取到Experience的指向
+	// 
+	if (!ExperienceId.IsValid() && UGameplayStatics::HasOption(OptionsString, TEXT("Experience")))
+	{
+		const FString ExperienceFromOptions = UGameplayStatics::ParseOption(OptionsString, TEXT("Experience"));
+		ExperienceId = FPrimaryAssetId(FPrimaryAssetType(ULyraExperienceDefinition::StaticClass()->GetFName()), FName(*ExperienceFromOptions));
+		ExperienceIdSource = TEXT("OptionsString");
+	}
+
+	// 3.开发者设置（仅适用于 PIE）
+	if (!ExperienceId.IsValid() && World->IsPlayInEditor())
+	{
+		ExperienceId = GetDefault<ULyraDeveloperSettings>()->ExperienceOverride;
+		ExperienceIdSource = TEXT("DeveloperSettings");
+	}
+	
+	// 4.命令行覆盖
+	// see if the command line wants to set the experience
+	// 查看命令行是否想要设置体验参数
+	if (!ExperienceId.IsValid())
+	{
+		FString ExperienceFromCommandLine;
+		if (FParse::Value(FCommandLine::Get(), TEXT("Experience="), ExperienceFromCommandLine))
+		{
+			ExperienceId = FPrimaryAssetId::ParseTypeAndName(ExperienceFromCommandLine);
+
+			if (!ExperienceId.PrimaryAssetType.IsValid())
+			{
+				ExperienceId = FPrimaryAssetId(FPrimaryAssetType(ULyraExperienceDefinition::StaticClass()->GetFName()), FName(*ExperienceFromCommandLine));
+
+				
+			}
+			ExperienceIdSource = TEXT("CommandLine");
+		}
+
+	}
+	
+	// 5.世界设置覆盖
+	// 查看世界设置中是否有默认的体验设置
+	// see if the world settings has a default experience
+	if (!ExperienceId.IsValid())
+	{
+		if (ALyraWorldSettings* TypedWorldSettings = Cast<ALyraWorldSettings>(GetWorldSettings()))
+		{
+			ExperienceId = TypedWorldSettings->GetDefaultGameplayExperience();
+			ExperienceIdSource = TEXT("WorldSettings");
+		}
+	}
+	
+	// 6.资产管理器覆盖
+	ULyraAssetManager& AssetManager = ULyraAssetManager::Get();
+	FAssetData Dummy;
+	if (ExperienceId.IsValid() && !AssetManager.GetPrimaryAssetData(ExperienceId, /*out*/ Dummy))
+	{
+		UE_LOG(LogLyraExperience, Error, TEXT("EXPERIENCE: Wanted to use %s but couldn't find it, falling back to the default)"), *ExperienceId.ToString());
+		ExperienceId = FPrimaryAssetId();
+	}
+	
+	// Final fallback to the default experience
+	// 最终将采用默认体验模式
+	if (!ExperienceId.IsValid())
+	{
+		// 7.专属服务器体验覆盖
+		if (TryDedicatedServerLogin())
+		{
+			// This will start to host as a dedicated server
+			// 这将开始作为独立服务器进行运行
+			return;
+		}	
+		
+		
+
+		//@TODO: Pull this from a config setting or something
+		//@待办事项：从配置设置或其他地方获取此内容
+		ExperienceId = FPrimaryAssetId(FPrimaryAssetType("LyraExperienceDefinition"), FName("B_LyraDefaultExperience"));
+		ExperienceIdSource = TEXT("Default");
+	}
+	
+
+	OnMatchAssignmentGiven(ExperienceId, ExperienceIdSource);
+}
+
+bool ALyraGameMode::TryDedicatedServerLogin()
+{
+	// Some basic code to register as an active dedicated server, this would be heavily modified by the game
+	// 一些用于注册为活跃专用服务器的基本代码，这些代码会由游戏进行大幅修改
+	FString DefaultMap = UGameMapsSettings::GetGameDefaultMap();
+	UWorld* World = GetWorld();
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance && World && World->GetNetMode() == NM_DedicatedServer && World->URL.Map == DefaultMap)
+	{
+
+		// Only register if this is the default map on a dedicated server
+		// 仅在该服务器为专用服务器且此为默认地图的情况下才进行注册
+		UCommonUserSubsystem* UserSubsystem = GameInstance->GetSubsystem<UCommonUserSubsystem>();
+
+		// Dedicated servers may need to do an online login
+		// 专用服务器可能需要进行在线登录操作
+		UserSubsystem->OnUserInitializeComplete.AddDynamic(this, &ALyraGameMode::OnUserInitializedForDedicatedServer);
+
+		// There are no local users on dedicated server, but index 0 means the default platform user which is handled by the online login code
+		// 在专用服务器上没有本地用户，但索引 0 表示默认的平台用户，该用户由在线登录代码进行处理。
+		if (!UserSubsystem->TryToLoginForOnlinePlay(0))
+		{
+			OnUserInitializedForDedicatedServer(nullptr, false, FText(), ECommonUserPrivilege::CanPlayOnline, ECommonUserOnlineContext::Default);
+		
+		}
+		
+		return true;
+
+		
+	}
+		
+
+	return false;
+}
+
+void ALyraGameMode::HostDedicatedServerMatch(ECommonSessionOnlineMode OnlineMode)
+{
+	FPrimaryAssetType UserExperienceType = ULyraUserFacingExperienceDefinition::StaticClass()->GetFName();
+
+	// Figure out what UserFacingExperience to load
+	// 确定要加载的用户可见体验内容
+	
+	FPrimaryAssetId UserExperienceId;
+	FString UserExperienceFromCommandLine;
+
+	// 通过命令行解析
+	if (FParse::Value(FCommandLine::Get(), TEXT("UserExperience="), UserExperienceFromCommandLine) ||
+		FParse::Value(FCommandLine::Get(), TEXT("Playlist="), UserExperienceFromCommandLine))
+	{
+		UserExperienceId = FPrimaryAssetId::ParseTypeAndName(UserExperienceFromCommandLine);
+		
+		if (!UserExperienceId.PrimaryAssetType.IsValid())
+		{
+			UserExperienceId = FPrimaryAssetId(FPrimaryAssetType(UserExperienceType), FName(*UserExperienceFromCommandLine));
+		}
+
+	}
+	
+	// Search for the matching experience, it's fine to force load them because we're in dedicated server startup
+	// 寻找匹配的体验内容，可以强制加载这些内容，因为我们正处于专用服务器启动阶段。
+	ULyraAssetManager& AssetManager = ULyraAssetManager::Get();
+	TSharedPtr<FStreamableHandle> Handle = AssetManager.LoadPrimaryAssetsWithType(UserExperienceType);
+
+	if (ensure(Handle.IsValid()))
+	{
+		Handle->WaitUntilComplete();
+	}
+
+	// 全部加载进来
+	TArray<UObject*> UserExperiences;
+	AssetManager.GetPrimaryAssetObjectList(UserExperienceType, UserExperiences);
+	
+	ULyraUserFacingExperienceDefinition* FoundExperience = nullptr;
+	ULyraUserFacingExperienceDefinition* DefaultExperience = nullptr;
+	
+	// 寻找指定的和默认的
+	for (UObject* Object : UserExperiences)
+	{
+		ULyraUserFacingExperienceDefinition* UserExperience = Cast<ULyraUserFacingExperienceDefinition>(Object);
+		if (ensure(UserExperience))
+		{
+
+			if (UserExperience->GetPrimaryAssetId() == UserExperienceId)
+			{
+				FoundExperience = UserExperience;
+				break;
+			}
+
+			if (UserExperience->bIsDefaultExperience && DefaultExperience == nullptr)
+			{
+				DefaultExperience = UserExperience;
+			}
+			
+		}
+	
+	}
+
+	// 如果指定的没有,那就使用默认的
+	if (FoundExperience == nullptr)
+	{
+		FoundExperience = DefaultExperience;
+	}
+
+	UGameInstance* GameInstance = GetGameInstance();
+
+	if (ensure(FoundExperience && GameInstance))
+	{
+
+		// Actually host the game
+		// 实际上举办比赛
+		
+		UCommonSession_HostSessionRequest* HostRequest = FoundExperience->CreateHostingRequest(this);
+
+		
+		if (ensure(HostRequest))
+		{
+			//线上模式,所以是专属服务器
+			HostRequest->OnlineMode = OnlineMode;
+
+			// TODO override other parameters?
+			// 请务必修改其他参数吗？
+
+			UCommonSessionSubsystem* SessionSubsystem = GameInstance->GetSubsystem<UCommonSessionSubsystem>();
+			SessionSubsystem->HostSession(nullptr, HostRequest);
+			
+			// This will handle the map travel
+			// 这将负责地图上的移动操作
+		}
+
+		
+	}
+	
+	
+	
+
+	
+}
+
+void ALyraGameMode::OnUserInitializedForDedicatedServer(const UCommonUserInfo* UserInfo, bool bSuccess, FText Error,
+                                                        ECommonUserPrivilege RequestedPrivilege, ECommonUserOnlineContext OnlineContext)
+{
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance)
+	{
+		// Unbind
+		// 解除绑定
+		UCommonUserSubsystem* UserSubsystem = GameInstance->GetSubsystem<UCommonUserSubsystem>();
+		UserSubsystem->OnUserInitializeComplete.RemoveDynamic(this, &ALyraGameMode::OnUserInitializedForDedicatedServer);
+
+		// Dedicated servers do not require user login, but some online subsystems may expect it
+		// 专用服务器无需用户登录，但某些在线子系统可能仍要求用户登录。
+		if (bSuccess && ensure(UserInfo))
+		{
+			UE_LOG(LogLyraExperience, Log, TEXT("Dedicated server user login succeeded for id %s, starting online server"), *UserInfo->GetNetId().ToString());
+		}
+		else
+		{
+			UE_LOG(LogLyraExperience, Log, TEXT("Dedicated server user login unsuccessful, starting online server as login is not required"));
+		}
+
+		
+		HostDedicatedServerMatch(ECommonSessionOnlineMode::Online);
+		
+	}
+
+
+
+	
+}
--- Lyra/Source/LyraGame/GameModes/LyraGameMode.h
+++ Lyra/Source/LyraGame/GameModes/LyraGameMode.h
@@ -22,6 +22,7 @@ struct FFrame;
 struct FPrimaryAssetId;
 enum class ECommonSessionOnlineMode : uint8;
 
+
 /**
  * Post login event, triggered when a player or bot joins the game as well as after seamless and non seamless travel
  *
@@ -31,7 +32,6 @@ enum class ECommonSessionOnlineMode : uint8;
  * 这是在玩家完成初始化操作之后所执行的程序。
  */
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnLyraGameModePlayerInitialized, AGameModeBase* /*GameMode*/, AController* /*NewPlayer*/);
-
 
 
 /**
@@ -49,6 +49,162 @@ public:
 
 	//构造函数 用于指定项目使用的默认类
 	UE_API ALyraGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	/*
+	 * 从控制器拿到PawnData
+	 * 可以AI控制器,也可以是玩家控制器
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Lyra|Pawn")
+	UE_API const ULyraPawnData* GetPawnDataForController(const AController* InController) const;
+
+
+	
+	//~AGameModeBase interface
+	/**
+	 * 初始化游戏。
+	 * 在调用任何其他函数（包括 PreInitializeComponents() 函数）之前，会调用 GameMode 的 InitGame() 事件。
+	 * 这个事件由 GameMode 使用来初始化参数并生成其辅助类。
+	 * 注意：此事件在角色的 PreInitializeComponents 事件之前被调用。
+	 */
+	UE_API virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+
+
+	/**
+	 * 返回给定控制器的默认兵类
+	 * 我们重写了这个方法,当我们加载体验完成之后PawnData就应该可用了.通过它获取
+	 * 初始化的时候,可能没有加载完成,那就现用默认的
+	 * 
+	 */
+	UE_API virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+	
+	/**
+	 * 重写这个函数是为了在生成过程中将PawnData的数据注入，（在出生地playerstart 和使用的类型确定了之后，会执行这里开始spawn
+	 * 在“重启玩家”过程中调用此函数以实际生成玩家的兵种，当使用变换时
+	 * @参数 新玩家 - 生成兵种的控制器
+	 * @参数 生成变换 - 用于生成兵种的变换体
+	 * @返回 一个默认兵种类的兵种
+	 * 
+	 */
+	UE_API virtual APawn* SpawnDefaultPawnAtTransform_Implementation(AController* NewPlayer, const FTransform& SpawnTransform) override;
+	
+	/** 如果“查找玩家起点”操作应使用玩家身上所保存的“起点位置”而非调用“选择玩家起点”功能，则返回 true */
+	UE_API virtual bool ShouldSpawnAtStartSpot(AController* Player) override;
+	
+	/** 表示玩家已准备好进入游戏，此时游戏可能会启动 */
+	UE_API virtual void HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer) override;
+
+	/**
+	 * 返回此玩家可从何处开始生成的“最佳”位置
+	 * 默认实现会寻找一个随机且未被占用的位置*
+	 * @参数 Player：我们为它所选择的控制角色
+	 * @返回值：被选作玩家起始位置的 AActor 对象（通常为 PlayerStart 类型）
+	 * 转发给LyraPlayerSpawningManagerComponent
+	 *	 
+	 */
+	UE_API virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+	
+
+	/**
+	 * 处理重新播放玩家的后半部分内容
+	 * 转发给LyraPlayerSpawningManagerComponent
+	 */
+	UE_API virtual void FinishRestartPlayer(AController* NewPlayer, const FRotator& StartRotation) override;
+
+	/**
+	 * 若调用“重启玩家”操作是有效的，则返回 true。默认情况下会调用“玩家”的“可以重启玩家”方法
+	 * 例如设置一个机器人只有3条命，3次死亡后就不能重启
+	 * 转发给LyraPlayerSpawningManagerComponent
+	 */
+	UE_API virtual bool PlayerCanRestart_Implementation(APlayerController* Player) override;
+	
+	// 绑定体验加载完成之后回调
+	UE_API virtual void InitGameState() override;
+
+
+	/**
+	 * 尝试初始化玩家的“起始位置”。
+	 * @参数  Player  玩家的控制器。
+	 * @参数  OutErrorMessage  任何错误信息。
+	 * @返回  bool  如果更新了起始位置则返回 true，否则返回 false。
+	 * 在GameModeBase的AGameModeBase::Login->InitNewPlayer()调用.时机太早了.我们用不上
+	 * 
+	 */
+	UE_API virtual bool UpdatePlayerStartSpot(AController* Player, const FString& Portal, FString& OutErrorMessage) override;
+
+	/**
+	 * 负责所有在旅行方式之间共享的玩家初始化工作
+	 * （即从 PostLogin() 和 HandleSeamlessTravelPlayer() 中调用）*
+	 * 
+	 */
+	UE_API virtual void GenericPlayerInitialization(AController* NewPlayer) override;
+
+	UE_API virtual void FailedToRestartPlayer(AController* NewPlayer) override;
+
+
+	// Restart (respawn) the specified player or bot next frame
+	// - If bForceReset is true, the controller will be reset this frame (abandoning the currently possessed pawn, if any)
+	UFUNCTION(BlueprintCallable)
+	UE_API void RequestPlayerRestartNextFrame(AController* Controller, bool bForceReset = false);
+
+
+	
+	//~End of AGameModeBase interface
+	
+	/*
+	 * Agnostic version of PlayerCanRestart that can be used for both player bots and players
+	 * “无神论版”的“玩家可重置”功能，可用于玩家机器人和玩家自身。
+	 * 转发给LyraPlayerSpawningManagerComponent
+	 * 
+	 */
+	UE_API virtual bool ControllerCanRestart(AController* Controller);
+
+
+	// Delegate called on player initialization, described above
+	// 在玩家初始化时调用的委托函数，如上文所述
+	FOnLyraGameModePlayerInitialized OnGameModePlayerInitialized;
+
+
+
+protected:
+	/*
+	 * 体验加载完成后回调
+	 * 它的调用可能晚于原引擎的玩家生成流程,所以需要重设玩家状态
+	 * 在InitGameState中绑定.
+	 */
+	UE_API void OnExperienceLoaded(const ULyraExperienceDefinition* CurrentExperience);
+	// 体验是否加载完成 通过访问GameState的体验管理插件得知
+	UE_API bool IsExperienceLoaded() const;
+
+	
+	/*
+	 * 确定体验之后调用GameState的体验管理组件,在服务端设置体验,开始体验的加载流程.
+	 * 
+	 */
+	UE_API void OnMatchAssignmentGiven(FPrimaryAssetId ExperienceId, const FString& ExperienceIdSource);
+
+
+	
+	/*
+	 * 重载体验experience 的选型
+	 * 在InitGame的绑定到下一帧调用执行
+	 * 包含两种路径,一种是用户自己拉起来游戏,还有一种是DS举行在线服务.
+	 * 
+	 */
+	UE_API void HandleMatchAssignmentIfNotExpectingOne();
+
+
+	// 专属服务器情况开始自动登录 等待登录结果 去触发体验重写开始主持游戏
+	UE_API bool TryDedicatedServerLogin();
+	UE_API void HostDedicatedServerMatch(ECommonSessionOnlineMode OnlineMode);
+
+	/*
+	 * 专属服务器情况下,本地用户注册登录的结果回调
+	 */
+	UFUNCTION()
+	UE_API void OnUserInitializedForDedicatedServer(const UCommonUserInfo* UserInfo,
+		bool bSuccess, FText Error, ECommonUserPrivilege RequestedPrivilege, ECommonUserOnlineContext OnlineContext);
+	
 
 };
 
