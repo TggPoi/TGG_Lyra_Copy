43fceaa2ef002fe3a593526c545374faa911e323
TggPoi <2631925996@qq.com>
2025-11-01 16:32:32 +0800

    9.1-UI启动报错修复

--- Lyra/Source/LyraGame/UI/Subsystem/LyraUIManagerSubsystem.h
+++ Lyra/Source/LyraGame/UI/Subsystem/LyraUIManagerSubsystem.h
@@ -1,3 +1,37 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+// Finished.
 #pragma once
+
+#include "Containers/Ticker.h"
+#include "GameUIManagerSubsystem.h"
+
+
+#include "LyraUIManagerSubsystem.generated.h"
+
+class FSubsystemCollectionBase;
+class UObject;
+UCLASS()
+class ULyraUIManagerSubsystem : public UGameUIManagerSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	ULyraUIManagerSubsystem();
+
+	// 绑定Tick
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	// 移除Tick
+	virtual void Deinitialize() override;
+
+private:
+	// Tick
+	bool Tick(float DeltaTime);
+	
+	// 更改UI布局的可实现
+	void SyncRootLayoutVisibilityToShowHUD();
+	
+	FTSTicker::FDelegateHandle TickHandle;
+
+};
