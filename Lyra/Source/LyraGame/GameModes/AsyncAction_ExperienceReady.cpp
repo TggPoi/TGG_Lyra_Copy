a61e75f2976aef0a7b1131f97083a4b6d7e5384d
TggPoi <2631925996@qq.com>
2025-11-01 17:39:05 +0800

    9.2-蓝图异步节点-异步询问加载

--- Lyra/Source/LyraGame/GameModes/AsyncAction_ExperienceReady.cpp
+++ Lyra/Source/LyraGame/GameModes/AsyncAction_ExperienceReady.cpp
@@ -1,3 +1,125 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "AsyncAction_ExperienceReady.h"
+
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "GameModes/LyraExperienceManagerComponent.h"
+#include "TimerManager.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AsyncAction_ExperienceReady)
+
+UAsyncAction_ExperienceReady::UAsyncAction_ExperienceReady(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+
+UAsyncAction_ExperienceReady* UAsyncAction_ExperienceReady::WaitForExperienceReady(UObject* InWorldContextObject)
+{
+	UAsyncAction_ExperienceReady* Action = nullptr;
+
+	if (UWorld* World = GEngine->GetWorldFromContextObject(InWorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		Action = NewObject<UAsyncAction_ExperienceReady>();
+		Action->WorldPtr = World;
+		
+		// 这里是注册到了世界上，避免被GC回收，让这个对象的生命周期和世界（游戏实例）保持一致
+		Action->RegisterWithGameInstance(World);
+	}
+	return Action;
+}
+
+void UAsyncAction_ExperienceReady::Activate()
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		if (AGameStateBase* GameState = World->GetGameState())
+		{
+			// 有GameState 直接通过GameState实现
+			Step2_ListenToExperienceLoading(GameState);
+		}
+		else
+		{
+			// GameState还没生成,绑定在GameState生成事件上
+			World->GameStateSetEvent.AddUObject(this, &ThisClass::Step1_HandleGameStateSet);
+		}
+		
+	}
+	else
+	{
+		// No world so we'll never finish naturally
+		// 没有世界,无法执行,直接销毁
+		SetReadyToDestroy();
+	}
+	
+
+	
+}
+
+void UAsyncAction_ExperienceReady::Step1_HandleGameStateSet(AGameStateBase* GameState)
+{
+	if (UWorld* World = WorldPtr.Get())
+	{	// 清楚之前来自我这个对象的绑定.
+		World->GameStateSetEvent.RemoveAll(this);
+	}
+
+	Step2_ListenToExperienceLoading(GameState);
+	
+}
+
+void UAsyncAction_ExperienceReady::Step2_ListenToExperienceLoading(AGameStateBase* GameState)
+{
+	check(GameState);
+	ULyraExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<ULyraExperienceManagerComponent>();
+	check(ExperienceComponent);
+	// 是否已经加载了 如果加载了就直接调用 但是直接调用可能会有主逻辑问题 那么最好手动做一帧的延迟
+	// 没有加载就绑定在代理上即可
+	if (ExperienceComponent->IsExperienceLoaded())
+	{
+		UWorld* World = GameState->GetWorld();
+		check(World);
+
+		// The experience happened to be already loaded, but still delay a frame to
+		// make sure people don't write stuff that relies on this always being true
+
+		// 这段代码中的逻辑已经得到了执行，但为了确保不会出现因某些操作依赖于该逻辑始终为真而导致的问题，还是再延迟了一帧。
+
+		
+		//@TODO: Consider not delaying for dynamically spawned stuff / any time after the loading screen has dropped?
+		//@TODO: Maybe just inject a random 0-1s delay in the experience load itself?
+
+		//@待办事项：考虑不要为动态生成的内容或在加载屏幕消失之后的任何时间点进行延迟处理？
+		//@待办事项：或许可以在游戏加载过程中直接插入一个 0 到 1 秒的随机延迟？
+
+		
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this, &ThisClass::Step4_BroadcastReady));
+		
+
+		
+	}
+	else
+	{
+		ExperienceComponent->CallOrRegister_OnExperienceLoaded(FOnLyraExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::Step3_HandleExperienceLoaded));
+
+	}
+	
+	
+}
+
+void UAsyncAction_ExperienceReady::Step3_HandleExperienceLoaded(const ULyraExperienceDefinition* CurrentExperience)
+{
+	// 在这里可以获取到Experience 做进一步处理
+	// 目前没有特定的需求,直接触发代理即可!
+	Step4_BroadcastReady();
+	
+}
+
+void UAsyncAction_ExperienceReady::Step4_BroadcastReady()
+{
+	// 触发代理!
+	OnReady.Broadcast();
+
+	
+	SetReadyToDestroy();
+}
--- Lyra/Source/LyraGame/GameModes/AsyncAction_ExperienceReady.h
+++ Lyra/Source/LyraGame/GameModes/AsyncAction_ExperienceReady.h
@@ -1,3 +1,68 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+// Finished.
 #pragma once
+
+#include "Kismet/BlueprintAsyncActionBase.h"
+
+#include "AsyncAction_ExperienceReady.generated.h"
+
+class AGameStateBase;
+class ULyraExperienceDefinition;
+class UWorld;
+struct FFrame;
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FExperienceReadyAsyncDelegate);
+
+/**
+ * Asynchronously waits for the game state to be ready and valid and then calls the OnReady event.  Will call OnReady
+ * immediately if the game state is valid already.
+ *
+ * 异步等待游戏状态准备好且有效，然后调用“就绪”事件。  如果游戏状态已经有效，则会立即调用“就绪”事件。
+ *
+ * 如果要实现一个异步任务，一般不会把任务绑定在U类或者A类
+ * U类的生命周期太复杂，并且会创建CDO对象，增加运行负担；A类生命周期和场景绑定，切换场景容易出问题
+ * 推荐做法是创建一个插件单独处理异步任务，然后在需要的时候调用这个插件的异步任务，通过第三方库封装的方式
+ */
+
+
+UCLASS()
+class UAsyncAction_ExperienceReady : public UBlueprintAsyncActionBase
+{
+	GENERATED_UCLASS_BODY()
+	
+public:
+	// Waits for the experience to be determined and loaded
+	// 等待体验结果被确定并加载完成
+	UFUNCTION(BlueprintCallable, meta=(WorldContext = "WorldContextObject", BlueprintInternalUseOnly="true"))
+	static UAsyncAction_ExperienceReady* WaitForExperienceReady(UObject* WorldContextObject);
+
+	// 自动激活,由蓝图调用.如果是C++,请自行调用
+	virtual void Activate() override;
+
+	
+public:
+
+	// Called when the experience has been determined and is ready/loaded
+	// 当体验内容已确定并准备就绪/加载完成时触发此事件
+	UPROPERTY(BlueprintAssignable)
+	FExperienceReadyAsyncDelegate OnReady;
+
+
+private:
+	// 步骤1 设置GameState
+	void Step1_HandleGameStateSet(AGameStateBase* GameState);
+
+	// 步骤2 绑定事件
+	void Step2_ListenToExperienceLoading(AGameStateBase* GameState);
+	
+	// 步骤3 处理Experience加载完毕
+	void Step3_HandleExperienceLoaded(const ULyraExperienceDefinition* CurrentExperience);
+
+	// 步骤四 广播并销毁自身
+	void Step4_BroadcastReady();
+	
+
+	TWeakObjectPtr<UWorld> WorldPtr;
+
+};
